5d1f72b1fbb808e0281bf98cdeb3aac0af07dc2a
mariorecinos <recinos.mario68@gmail.com>
2024-10-31 05:38:17 -0700

    j

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,7 +22,6 @@ npm start &
 sleep 1
 echo $! > .pidfile
 set +x
-
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
